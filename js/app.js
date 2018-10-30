15ef3d4ad6f89519b073b9362e0648c3c3438c78
Nitish Singh <nitish_singh@apple.com>
2018-10-30 10:25:39 +0530

    Javascript change

--- js/app.js
+++ js/app.js
@@ -1,29 +1,75 @@
 
-
-const oReq = new XMLHttpRequest();
-oReq.addEventListener("load", loadJSONData);
-oReq.open("GET", "json/data.json");
-oReq.send();
-function loadJSONData() {
-  let data = JSON.parse(this.responseText);
-  getTeam(data);
-}
-
-function getTeam(list) {
-  const body = document.getElementById("team");
-  let teamData = list.map((val, i, list) => {
-    return dataList(list[i].team, body);
-  });
-}
-
-
-
-function dataList (val, body){
-    let options = document.createElement("option");
-    options.setAttribute("value", val);
-    body.appendChild(options);
-}
-
 (function () {
-  getTeam();
+  loadJSON();
+
+let jsondata = undefined;
+//Loading json file
+function loadJSON() {
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'json/data.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      jsondata = JSON.parse(xobj.responseText);
+      getTeam();
+    }
+  };
+  xobj.send(null);
+}
+
+//Availble team
+function getTeam() {
+  const body = document.getElementById("team");
+  jsondata.map((val, i, data) => {
+      return dataList(data[i].team, body);
+    });
+}
+
+//carete html
+function dataList(val, body) {
+  if( Array.isArray(val)){
+  val.forEach(iteam => {
+  let options = document.createElement("option");
+  options.setAttribute("value", iteam);
+  body.appendChild(options);
+ });
+}else{
+  let options = document.createElement("option");
+  options.setAttribute("value", val);
+  body.appendChild(options);
+}
+}
+  let teameName = document.querySelector("#orgTeam");
+  teameName.onblur = function(){
+  let optionVal = false;
+  let datalist = teameName.list;
+  for (let i = 0; i < datalist.options.length; i++) {
+    if (teameName.value.toUpperCase() === datalist.options[i].value.toUpperCase()) {
+      optionVal = true;
+      break;
+    }
+  }
+  let emp = document.getElementById("orgEmployee");
+  if (optionVal) {
+    emp.removeAttribute("disabled");
+   
+      const ebody = document.getElementById("employee");
+      ebody.innerHTML="";
+      jsondata.forEach(team => {
+        
+        if(team.team.indexOf(teameName.value) > -1){
+         dataList(team.employees, ebody);
+        }
+      });
+  } else {
+    console.log('Please select a valid value.');
+    emp.setAttribute("disabled", true);
+  }
+  }
+
 })();
+
+
+
+
+
